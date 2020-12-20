2b658635d587cb26584254e124d4e121d93a53f1
mdk-klm <antoine.04@laposte.net>
2020-12-20 18:11:26 +0100

    ajout d'un composant

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Table from './Table'
+import OtherTable from './OtherTable'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    render() {
+        const characters = [
+            {
+              name: 'Charlie',
+              job: 'Janitor',
+            },
+            {
+              name: 'Mac',
+              job: 'Bouncer',
+            },
+            {
+              name: 'Dee',
+              job: 'Aspring actress',
+            },
+            {
+              name: 'Dennis',
+              job: 'Bartender',
+            },
+          ]
 
-export default App;
+          const animals =[
+              {
+                  name :'georges',
+                  genre :'dog'
+              },
+              {
+                  name :'fabrice',
+                  genre : 'cat'
+              },
+          ]
+      
+          return (
+            <div className="container">
+              <Table characterData ={characters} />
+              <OtherTable animalData = {animals}/>
+    
+            </div>
+        
+      )
+    }
+  }
+
+  
+export default App
