6844c58a1687b190e953ab1dc94e6d9671efb756
mdk-klm <antoine.04@laposte.net>
2020-12-20 23:00:49 +0100

    add Form component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
-import React, {Component} from 'react'
-import Table from './Table'
-import OtherTable from './OtherTable'
+import React, { Component } from "react";
+import Table from "./Table";
+import OtherTable from "./OtherTable";
+import Form from "./Form";
 
 class App extends Component {
-    render() {
-        const characters = [
-            {
-              name: 'Charlie',
-              job: 'Janitor',
-            },
-            {
-              name: 'Mac',
-              job: 'Bouncer',
-            },
-            {
-              name: 'Dee',
-              job: 'Aspring actress',
-            },
-            {
-              name: 'Dennis',
-              job: 'Bartender',
-            },
-          ]
+  state = {
+    characters: [],
 
-          const animals =[
-              {
-                  name :'georges',
-                  genre :'dog'
-              },
-              {
-                  name :'fabrice',
-                  genre : 'cat'
-              },
-          ]
-      
-          return (
-            <div className="container">
-              <Table characterData ={characters} />
-              <OtherTable animalData = {animals}/>
-    
-            </div>
-        
-      )
-    }
+    animals: [
+      {
+        name: "georges",
+        genre: "dog",
+      },
+      {
+        name: "fabrice",
+        genre: "cat",
+      },
+    ],
+  };
+
+  removeCharacter = (index) => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      }),
+    });
+  };
+
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
+  render() {
+    const { characters } = this.state;
+    const { animals } = this.state;
+
+    return (
+      <div className="container">
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+        <OtherTable animalData={animals} />
+        <Form handleSubmit={this.handleSubmit}/>
+      </div>
+    );
   }
+}
 
-  
-export default App
+export default App;
--- src/OtherTable.js
+++ src/OtherTable.js
@@ -1,38 +1,38 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 const OthertableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Kind</th>
+      </tr>
+    </thead>
+  );
+};
+const OthertableBody = (props) => {
+  const rowAnimal = props.animalData.map((row, index) => {
     return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Kind</th>
-        </tr>
-      </thead>
-    )
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.genre}</td>
+      </tr>
+    );
+  });
+
+  return <tbody>{rowAnimal}</tbody>;
+};
+
+class OtherTable extends Component {
+  render() {
+    const { animalData } = this.props;
+    return (
+      <table>
+        <OthertableHeader />
+        <OthertableBody animalData={animalData} />
+      </table>
+    );
   }
-  const OthertableBody = (props) => {
-      const rowAnimal = props.animalData.map((row, index) => {
-        return (
-            <tr key={index}>
-              <td>{row.name}</td>
-              <td>{row.genre}</td>
-            </tr>
-          )
-        })
+}
 
-        return <tbody>{rowAnimal}</tbody>
-      }
-
-      class OtherTable extends Component {
-          render(){
-              const {animalData} = this.props
-              return (
-                <table>
-                  <OthertableHeader />
-                  <OthertableBody animalData = {animalData} />
-                </table>
-              )
-          }
-      }
-  
-      export default OtherTable;
+export default OtherTable;
